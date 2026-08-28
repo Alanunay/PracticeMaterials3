685dd916cd163de01c7bfb15a43af14a9d813a21
Yeuur <ajcalidguid12345@gmail.com>
2026-08-28 16:01:55 +0800

    Add LoginCard component for user authentication and messaging

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
-import { Counter } from './Counter'
+//import { Counter } from './Counter'
+import { LoginCard } from './LoginCard'
 function App() {
   return (
     <>
       <h1><strong>My React Practice</strong></h1>
-      <Counter/>
+      {/* <Counter/> */}
+      <LoginCard/>
     </>
   )
 }
