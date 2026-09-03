3591fdeddb64d89b8987b3ab0d8bba664852a317
Yeuur <ajcalidguid12345@gmail.com>
2026-09-03 11:10:00 +0800

    refactor: update SimpleCounter to improve logging and handleClick functionality

--- src/SimpleCounter.jsx
+++ src/SimpleCounter.jsx
@@ -1,17 +1,28 @@
 
 import {useState} from 'react'
 export default function SimpleCounter() {
-    console.log('SimpleCounter component rendered')
-  const [count, setCount] = useState(0)
-const handleIncrement = () => {
-    console.log("Before setCount, count is:", count);
-    setCount(count + 1)
-    console.log("After setCount, count is:", count);
+const [count, setCount] = useState(0)
+ console.log('Render phase: Component renderiung with count=', count);
+// const handleIncrement = () => {
+//     console.log("Before setCount, count is:", count);
+//     setCount(count + 1)
+//     console.log("Still in trigger phase. After setCount, count is:", count);
+// }
+const handleClick = () => {
+    setCount(count + 1);
+    console.log("After setCount(count + 1), count is:", count);
+    setCount(count + 5);
+    console.log("After setCount(count + 5), count is:", count);
+    setCount(count + 10);
+    console.log("After setCount(count + 10), count is:", count);
+    setTimeout(() => {
+        console.log("After 2 seconds, count is:", count);
+    }, 2000);
 }
   return (
     <div>
       <h2>Count: {count}</h2>
-       <button onClick={handleIncrement}>Increment</button>
+       <button onClick={handleClick}>Increment</button>
     </div>
   )
 }
